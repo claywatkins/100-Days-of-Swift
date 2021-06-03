9094e1e6c59cb5e625d8cea32eba9e3e6cc7b1b2
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-06-03 09:10:48 -0700

    Completed project 5 challenges

---
+++ Swift UI/WordScramble - Project 5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Clayton Watkins on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
